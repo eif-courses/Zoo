7cb07cd3d481976f663e7dbba96cbf49771f1521
Marius Gžegoževskis <m.gzegozevskis@eif.viko.lt>
2021-11-02 17:01:33 +0200

    Cleaned, Fixed default constructor

--- src/main/java/eif/viko/lt/zoo/HelloController.java
+++ src/main/java/eif/viko/lt/zoo/HelloController.java
@@ -20,16 +20,6 @@ import java.util.*;
 public class HelloController implements Initializable {
 
 
-    private final Image IMAGE_RUBY  = new Image("https://upload.wikimedia.org/wikipedia/commons/f/f1/Ruby_logo_64x64.png");
-    private final Image IMAGE_APPLE  = new Image("http://findicons.com/files/icons/832/social_and_web/64/apple.png");
-    private final Image IMAGE_VISTA  = new Image("http://antaki.ca/bloom/img/windows_64x64.png");
-    private final Image IMAGE_TWITTER = new Image("http://files.softicons.com/download/social-media-icons/fresh-social-media-icons-by-creative-nerds/png/64x64/twitter-bird.png");
-
-    private Image[] listOfImages = {IMAGE_RUBY, IMAGE_APPLE, IMAGE_VISTA, IMAGE_TWITTER};
-
-
-
-
     FirebaseAuth auth = null;
     DatabaseReference refUsers = FirebaseDatabase.getInstance().getReference("/users");
 
